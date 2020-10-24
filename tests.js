5fbc709cc169ad322b42bfa9e25b9a9af7370391
Tom de Bruijn <tom@tomdebruijn.com>
2020-10-24 15:19:56 +0200

    Add prototype app
    * Imagine there's a very detailed commit message here *

---
+++ tests.js
@@ -0,0 +1,47 @@
+function assertEqual(left, right) {
+  if (left === right) {
+    return;
+  }
+
+  throw new Error(`Assertion failure.\n  Left:  ${left}\n  Right: ${right}`);
+}
+
+const tests = [];
+
+function test(fn) {
+  tests.push(fn);
+};
+
+function runTests() {
+  const errors = [];
+  for(const testFn of tests) {
+    try {
+      testFn();
+      process.stdout.write(".");
+    } catch (e) {
+      errors.push(e)
+      process.stdout.write("X");
+    }
+  }
+
+  process.stdout.write("\n\n");
+  if (errors.length > 0) {
+    for (const error of errors) {
+      process.stdout.write(`${error.message}\n\n`);
+    }
+    process.stdout.write("Tests failed!");
+  } else {
+    process.stdout.write("Tests success!");
+  }
+  process.stdout.write("\n");
+}
+
+test(function() {
+  assertEqual(1, 1);
+});
+
+test(function() {
+  assertEqual(2, 2);
+});
+
+runTests();
